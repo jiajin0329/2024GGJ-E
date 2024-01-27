9d331ea480f628c3a3d6ea0991914f61a7550d29
jiajin0329 <98139948+jiajin0329@users.noreply.github.com>
2024-01-27 18:02:32 +0800

    build

--- Assets/Scripts/GameManager/GameManager.cs
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -5,6 +5,11 @@ public class GameManager : Singleton_MonoBehaviour<GameManager>
 {
     [SerializeField] public Info info;
 
+    private void Start()
+    {
+        
+    }
+
     public void InitPlayer()
     {
         foreach(var playerInfos in info.playerInfos)
@@ -12,6 +17,16 @@ public class GameManager : Singleton_MonoBehaviour<GameManager>
             playerInfos.controller.transform.position = playerInfos.startPosition;
             playerInfos.rigidbody2D.velocity = Vector3.zero;
             playerInfos.head.Reset();
+        }
+    }
+
+    private void SwitchControl()
+    {
+
+
+        foreach (var playerInfos in info.playerInfos)
+        {
+            playerInfos.controller.SwitchControl();
         }
     }
 }
